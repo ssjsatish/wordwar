e2be19cac3605cd068cc51af9a0447cb863f7c3a
Satish Patel <ssj.satish@gmail.com>
2020-12-19 12:17:56 +0530

    starting work of creating the header

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
       </header>
     </div>
   );
